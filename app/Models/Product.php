8671a715cc8c105fb9ef4f763f583faa90253aa7
marcellinus Yovian Indrastata <marcellinusyovian@gmail.com>
2025-10-26 08:07:25 +0700

    feat: create migration schema for product and stock movement. feat: install breeze for authentication

---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'sku',
+        'name',
+        'description',
+        'current_stock',
+    ];
+
+    protected $casts = [
+        'current_stock' => 'integer',
+    ];
+
+    public function stockMovements()
+    {
+        return $this->hasMany(StockMovement::class);
+    }
+}
